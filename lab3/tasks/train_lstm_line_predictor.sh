49e2d149963c86353b0c6b81589e9c14f99b2a8a
hoktay <hoktay@local>
2018-08-04 08:33:34 +0000

    More drop out.

--- lab3/tasks/train_lstm_line_predictor.sh
+++ lab3/tasks/train_lstm_line_predictor.sh
@@ -2,6 +2,6 @@
 pipenv run python training/run_experiment.py --save '{"dataset": "EmnistLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc",
 "train_args": {
             "batch_size": 64,
-            "epochs": 12
+            "epochs": 14
         }
         }'
